dbe2dc7de719003f25f1cd92fca19fa25acfc524
Lineth13 <100149446+Lineth13@users.noreply.github.com>
2022-04-27 10:46:20 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,10 @@
+while True:
+    
+    servos.P0.set_angle(0)
+    basic.pause(1000)
+    servos.P0.set_angle(90)
+    basic.pause(1000)
+    servos.P0.set_angle(180)
+    basic.pause(1000)
+    servos.P0.set_angle(90)
+    basic.pause(1000)
---
+++ main.ts
@@ -0,0 +1,10 @@
+while (true) {
+    servos.P0.setAngle(0)
+    basic.pause(1000)
+    servos.P0.setAngle(90)
+    basic.pause(1000)
+    servos.P0.setAngle(180)
+    basic.pause(1000)
+    servos.P0.setAngle(90)
+    basic.pause(1000)
+}
